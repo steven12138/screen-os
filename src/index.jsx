c81db2ef9867a359edc7ce013c748deb13b2f80d
steven12138 <m18611209860@163.com>
2020-11-05 13:54:24 +0800

    change somethig

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './views/Home';
+
+class PageCtrl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: "Home",
+        }
+    }
+    render() {
+        let element = undefined;
+        if (this.state.page == "Home") {
+            element = <Home />;
+        }
+        else if (this.state.page == "View") {
+            element = <div>View Page</div>;
+        }
+        else if (this.state.page == "admin") {
+            element = <div>Admin Page</div>;
+        }
+        return element;
+    }
+}
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <PageCtrl />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
---
+++ src/views/Home.jsx
@@ -0,0 +1,8 @@
+import classes from "*.module.scss";
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return <div>HomePage</div>;
+    }
+}
