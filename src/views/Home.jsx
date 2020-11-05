ae2eeccbbb50bfdee7b3d6badbe967757645b2a6
steven12138 <m18611209860@163.com>
2020-11-05 14:05:13 +0800

    just do it

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "../components/LoginBar";
 
 class Home extends React.Component {
     render() {
-        return <div>HomePage</div>;
+        return (
+            <Header />
+        );
     }
 }
 
